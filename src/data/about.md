4c5786141f5fc59dae4a6df27b906761df2929af
Christina <christinapadilla.cp@gmail.com>
2021-04-27 13:29:04 -0500

    import about.md

---
+++ src/data/about.md
@@ -0,0 +1,12 @@
+---
+metadata:  
+  - id: 3 
+  - version: '1.0.0' 
+  - featured: false 
+  - date_launched: '' 
+title: 'About'
+summary: 'A color contrast checker and library'
+thumbnail: ''
+---
+
+# About Christina
--- src/screens/About.js
+++ src/screens/About.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import aboutMe from '../data/about.md'
+import matter from 'gray-matter'
 
-const About = () => {
+const About = (props) => {
+  useEffect(() => {
+    // load md into react: https://stackoverflow.com/a/53975297/6055581
+    fetch(aboutMe)
+      .then((res) => res.text())
+      .then((text) => getMarkdown(text))
+      .catch((err) => console.error(err))
+
+    // eslint-disable-next-line
+  }, [])
+
+  const getMarkdown = (text) => {
+    //console.log('text', text)
+  }
   return (
     <div>
       <h2>About</h2>
